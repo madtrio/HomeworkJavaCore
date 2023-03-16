c49d2e68dc4e9200450b766300eca516376cd0b7
madtro <anna.kozitzyna@yandex.ru>
2023-03-16 22:17:14 +0300

    1

---
+++ src/Hw3/Bicycle.java
@@ -0,0 +1,11 @@
+package Hw3;
+
+public class Bicycle {
+
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}

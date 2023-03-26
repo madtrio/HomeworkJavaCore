842a24491f2348e47c0eaa4c120253cb7ca485b9
madtro <anna.kozitzyna@yandex.ru>
2023-03-26 21:10:41 +0300

    1

---
+++ src/Hw5/LoopChecker.java
@@ -0,0 +1,15 @@
+package Hw5;
+
+public class LoopChecker implements Checker {
+    private static final String ALLOWED_CHARACTERS = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890_";
+    @Override
+    public boolean check(String s) {
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if(ALLOWED_CHARACTERS.contains((Character.toString(chars[i])))){
+                return false;
+            }
+        }
+        return true;
+    }
+}

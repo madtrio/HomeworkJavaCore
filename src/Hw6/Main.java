ab6edf4064ef5c77805500b31f674dc6b0ecc7d8
madtro <anna.kozitzyna@yandex.ru>
2023-04-16 00:03:46 +0300

    2

--- src/Hw6/Main.java
+++ src/Hw6/Main.java
@@ -1,17 +1,17 @@
 package Hw6;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
     private static final Random RANDOM = new Random();
 
     public static void main(String[] args) {
-        List <Integer> nums = generate(10);
+        List <Integer> nums = generateInts(10);
+        List <String> words = generateStrings(10);
         System.out.println("Сгенерированные числа: " + nums);
         task1(nums);
-
+        task2(nums);
+        task3(List.of("test", "test", "test", "test", "TEST", "tes"));
     }
     public static void task1(List<Integer> nums) {
         System.out.println("Задача 1");
@@ -20,13 +20,45 @@ public class Main {
                 System.out.print(number + " ");
             }
         }
+        System.out.println();
     }
-    private static List<Integer> generate (int n) {
+    public static void task2(List<Integer> nums) {
+        System.out.println("Задача 2");
+        Set<Integer> set = new TreeSet<>(nums);
+        for (Integer number : set) {
+            if (number % 2 == 0) {
+                System.out.print(number + " ");
+            }
+        }
+        System.out.println();
+    }
+    public static void task3(List<String> words) {
+        System.out.println("Задача 3");
+        for (String word : words) {
+            if (Collections.frequency(words,word) == 1) {
+                System.out.print(word + " ");
+            }
+        }
+        System.out.println();
+    }
+    private static List<Integer> generateInts (int n) {
         List <Integer> list = new ArrayList<>(n);
         for (int i = 0; i < n ; i++) {
-            list.add(RANDOM.nextInt(5));
+            list.add(RANDOM.nextInt(10));
         }
         return list;
     }
-
+    private static List<String> generateStrings (int n) {
+        List <String> list = new ArrayList<>(n);
+        int length = 10;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < n ; i++) {
+            for (int j = 0; j < length; j++) {
+stringBuilder.append((char)RANDOM.nextInt('z'));
+            }
+            list.add(stringBuilder.toString());
+            stringBuilder.delete(0, stringBuilder.length());
+        }
+        return list;
+    }
 }
